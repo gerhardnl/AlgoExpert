308fb817a379b65f151a19e457bfc83f4c9efb9a
gerhardnl <64205396+gerhardnl@users.noreply.github.com>
2021-01-21 23:14:05 +0100

    MaxSumIncrisingSubsequence

--- src/com/company/MaxSumIncrisingSubsequence.java
+++ src/com/company/MaxSumIncrisingSubsequence.java
@@ -1,43 +1,49 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class MaxSumIncrisingSubsequence {
     public static List<List<Integer>> maxSumIncreasingSubsequence(int[] array) {
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> subSequence = new ArrayList<>();
+        List<Integer> subSequenceTemp = new ArrayList<>();
+        List<Integer> currentSubSequence = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
 
-        int lastAdded = 0;
-        int total = 0;
+        int total = Integer.MIN_VALUE;
+        int pointer = 0;
 
         for (int i = 0; i < array.length ; i++) {
-            List<Integer> temp = new ArrayList<>();
-            int tempTotal = 0;
-            temp.add(array[i]);
-            lastAdded = array[i];
-            tempTotal = array[i];
-
-            for (int j = 1 + i; j < array.length ; j++) {
-                if (array[j] > lastAdded){
-                    temp.add(array[j]);
-                    lastAdded = array[j];
-                    tempTotal += array[j];
+            int tempToAdd = array[i];
+            subSequenceTemp.add(-1);
+            for (int j = i; j >= 0 ; j--) {
+                if (array[j] < array[i] && tempToAdd - array[j] <= array[i]){
+                    tempToAdd = temp.get(j) + array[i];
+                    subSequenceTemp.set(i, j);
                 }
+                pointer = i;
             }
-            if (tempTotal > total){
-                subSequence = temp;
-                total = tempTotal;
+            temp.add(tempToAdd);
+            if(tempToAdd > total){
+                total = tempToAdd;
+                currentSubSequence.clear();
+                while (true){
+                    currentSubSequence.add(0,array[pointer]);
+                    pointer = subSequenceTemp.get(pointer);
+                    if (pointer == -1) break;
+                }
             }
         }
         result.add(List.of(total));
-        result.add(subSequence);
+        result.add(currentSubSequence);
 
         return result;
     }
 
     public static void main(String[] args) {
-        int[] array = {10,70,20,30,40,50,11,30};
+        int[] array = {-1,1};
         maxSumIncreasingSubsequence(array);
     }
 }
